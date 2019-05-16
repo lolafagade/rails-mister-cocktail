ee9b5e64a2a6e6ecf72e5dc1eab89a231c6a3445
Omolola Fagade <omololafagade@gmail.com>
2019-05-16 13:53:09 +0200

    Doses model added with references to cocktail and ingriendients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up the DB..."
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts "Creating ingredients..."
+
+ingredients_attribute = [
+  {
+    name: 'Rum'
+  },
+  {
+    name: 'Scotch'
+  },
+  {
+    name: 'Orange Bitters'
+  },
+  {
+    name: 'Creme de Cacao'
+  },
+  {
+    name: 'Egg yolk'
+  }
+]
+
+Ingredient.create!(ingredients_attribute)
+
+cocktails_attribute = [
+  {
+    name: 'Mojito'
+  },
+  {
+    name: 'Mint Julep'
+  },
+  {
+    name: 'Whiskey Sour'
+  },
+  {
+    name: 'Fruit Cocktail'
+  },
+  {
+    name: 'Virgin Cola'
+  }
+]
+
+Cocktail.create!(cocktails_attribute)
+
